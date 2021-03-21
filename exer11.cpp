1331b2a239f6506077e15559c5b83c612cd4a61f
Michael Evangelos Diamantis <michaeldiamantis17@outlook.com>
2021-03-21 18:35:40 +0200

    Exercise completed.

--- exer11.cpp
+++ exer11.cpp
@@ -5,19 +5,19 @@ int main()
 	int val1 = 10;
 	int val2 = 15;
 
-	int *pi = &val1; //Point to the address of val1;
+	int *pi = &val1; //Point to val1;
 	std::cout << "Old address: " << pi << " value: " << *pi << std::endl;
 
-	pi = &val2; //Point to the address of val2;
+	pi = &val2; //Point to val2;
 	std::cout << "New address: " << pi << " value: " << *pi << std::endl;
 
 	int val3 = 20;
 	int val4 = 25;
 	
-	int *pi2 = &val3;
+	int *pi2 = &val3; //Point to val3;
 	std::cout << "Old Value of val3: " << val3 << std::endl;
 
-	*pi2 = val4;
+	*pi2 = val4; //Set the value of val3 to the value of val4;
 	std::cout << "New Value of val3: " << val3 << std::endl;
 
 	return 0;
